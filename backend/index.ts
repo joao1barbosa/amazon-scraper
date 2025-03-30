60ec3e5fa4d80d038d4041ecb0f4daace9591251
joao1barbosa <joao1.barbosa@outlook.com>
2025-03-30 12:52:33 -0300

    initial commit

---
+++ backend/index.ts
@@ -0,0 +1,10 @@
+import { serve } from "bun";
+
+const server = serve({
+  port: 3000,
+  routes: {
+    "/": () => new Response("Welcome to Bun!"),
+  },
+});
+
+console.log(`Listening on localhost:${server.port}`);
